1c350283cd1190ca10bec08e982b3950bb0602f2
hw <huang_wei_2016@163.com>
2024-06-25 17:02:07 +0800

    feat：红包算法（随机+相等）

---
+++ lucky_money.go
@@ -0,0 +1,140 @@
+package lucky_money
+
+import (
+	"errors"
+	"fmt"
+	"github.com/shopspring/decimal"
+	"math/rand/v2"
+)
+
+var (
+	// MaxAmount 红包的最大金额，可自行定义
+	MaxAmount = decimal.NewFromFloat(200)
+	// MinAmount 红包的最小金额，可自行定义
+	MinAmount = decimal.NewFromFloat(1)
+	// Threshold 红包阈值
+	Threshold = MaxAmount.Sub(MinAmount)
+)
+
+const (
+	// Random 随机金额红包
+	Random = iota
+	// Identical 普通红包（每个红包金额相等）
+	Identical
+)
+
+type LuckyMoney struct {
+	// 数量
+	Quantity int32
+	// 总金额
+	Amount decimal.Decimal
+}
+
+type Checker interface {
+	Check() error
+}
+
+func (l *LuckyMoney) Check() error {
+	if l.Quantity <= 0 {
+		return errors.New("quantity must be greater than 0")
+	}
+	if l.Amount.LessThanOrEqual(decimal.Zero) {
+		return errors.New("amount must be greater than 'decimal.Zero'")
+	}
+	return nil
+}
+
+func (l *LuckyMoney) String() string {
+	return fmt.Sprintf("{Quantity:%d, Amount:%s}", l.Quantity, l.Amount)
+}
+
+func Split(l LuckyMoney, st int) ([]decimal.Decimal, error) {
+	if err := Check(l); err != nil {
+		return nil, err
+	}
+	switch st {
+	case Random:
+		return random(l)
+	case Identical:
+		return identical(l)
+	default:
+		return nil, errors.New("invalid st")
+	}
+}
+
+func SplitRandom(l LuckyMoney) ([]decimal.Decimal, error) {
+	return Split(l, Random)
+}
+
+func SplitIdentical(l LuckyMoney) ([]decimal.Decimal, error) {
+	return Split(l, Identical)
+}
+
+func random(l LuckyMoney) ([]decimal.Decimal, error) {
+	qd := decimal.NewFromInt32(l.Quantity)
+	if l.Amount.Equal(MaxAmount.Mul(qd)) {
+		return identical(l)
+	}
+	// 每个红包预留最小金额 #{MinAmount}
+	sm := l.Amount.Sub(MinAmount.Mul(qd))
+	if sm.Equal(decimal.Zero) {
+		return identical(l)
+	}
+	var rps = make([]decimal.Decimal, 0)
+	// 二倍拆分法，最后一个红包用减法，因为四舍五入存在金额丢失的问题
+	for l.Quantity > 1 && sm.GreaterThan(decimal.Zero) {
+		avg := sm.Div(qd)
+		n := decimal.NewFromFloat(rand.Float64()).Mul(avg).Mul(decimal.NewFromInt(2)).Truncate(2)
+		// 拆分出来的红包有可能超过最大阈值，超过则处理为最大金额
+		if n.GreaterThan(Threshold) {
+			n = Threshold
+		}
+		rps = append(rps, n.Add(MinAmount))
+		sm = sm.Sub(n)
+		l.Quantity--
+	}
+	// 判断最后一个是否超过最大金额
+	// 超过则随机分配到其他红包种
+	lst := sm.Add(MinAmount)
+	sub := lst.Sub(MaxAmount)
+	if sub.GreaterThan(decimal.Zero) {
+		lst = MaxAmount
+		// 将超出的金额随机分配到其他红包中
+		for {
+			i := rand.IntN(len(rps))
+			rp := rps[i].Add(sub)
+			if rp.LessThanOrEqual(MaxAmount) {
+				rps[i] = rp
+				break
+			}
+			rps[i] = MaxAmount
+			sub = rp.Sub(MaxAmount)
+		}
+	}
+	rps = append(rps, lst)
+	return rps, nil
+}
+
+func identical(l LuckyMoney) ([]decimal.Decimal, error) {
+	q := l.Quantity
+	rps := make([]decimal.Decimal, 0)
+	avg := l.Amount.Div(decimal.NewFromInt32(q)).Round(2)
+	for ; q > 1; q-- {
+		rps = append(rps, avg)
+	}
+	return append(rps, l.Amount.Sub(avg.Mul(decimal.NewFromInt32(l.Quantity-1)))), nil
+}
+
+func Check(l LuckyMoney) error {
+	if err := l.Check(); err != nil {
+		return err
+	}
+	qd := decimal.NewFromInt32(l.Quantity)
+	if l.Amount.Sub(qd.Mul(MinAmount)).LessThan(decimal.Zero) {
+		return errors.New("insufficient amount, should be greater than 'MinAmount'")
+	}
+	if l.Amount.Sub(MaxAmount.Mul(qd)).GreaterThan(decimal.Zero) {
+		return errors.New("overflow amount, should be less than 'MaxAmount'")
+	}
+	return nil
+}
---
+++ lucky_money_test.go
@@ -0,0 +1,191 @@
+package lucky_money
+
+import (
+	"github.com/shopspring/decimal"
+	"testing"
+)
+
+func TestSuccessLMSRandom(t *testing.T) {
+	lml := make([]LuckyMoney, 0)
+	lml = append(lml, LuckyMoney{Quantity: 33, Amount: decimal.NewFromFloat(33)})
+	lml = append(lml, LuckyMoney{Quantity: 99, Amount: decimal.NewFromFloat(101)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(32)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(1.01)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(11)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(2000)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(1988)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(1999)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(1500)})
+	for _, lm := range lml {
+		a := lm.Amount
+		split, err := SplitRandom(lm)
+		if err != nil {
+			t.Errorf("拆分红包失败 %s %s", lm.String(), err)
+			continue
+		}
+		sum := decimal.Zero
+		for _, d := range split {
+			if d.GreaterThan(MaxAmount) || d.LessThan(MinAmount) {
+				t.Errorf("红包金额异常 %s", d)
+			}
+			sum = sum.Add(d)
+		}
+		if !a.Equal(sum) {
+			t.Errorf("红包拆分异常 %s %s != %s", lm.String(), sum, a)
+		}
+	}
+}
+
+func TestErrorLMSRandom(t *testing.T) {
+	lml := make([]LuckyMoney, 0)
+	lml = append(lml, LuckyMoney{Quantity: -1, Amount: decimal.NewFromFloat(3)})
+	lml = append(lml, LuckyMoney{Quantity: 0, Amount: decimal.NewFromFloat(10)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(0)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(-1.01)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(0.89)})
+	lml = append(lml, LuckyMoney{Quantity: 3, Amount: decimal.NewFromFloat(2.99)})
+	lml = append(lml, LuckyMoney{Quantity: 100, Amount: decimal.NewFromFloat(99.999)})
+	lml = append(lml, LuckyMoney{Quantity: 9, Amount: decimal.NewFromFloat(1801)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(2500)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(2001)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(3000)})
+	fn := 0
+	for _, lm := range lml {
+		_, err := SplitRandom(lm)
+		if err != nil {
+			fn++
+		}
+	}
+	if fn != len(lml) {
+		t.Errorf("拆分红包失败: 预期失败用例 %d，实际失败用例 %d", fn, len(lml))
+	}
+}
+
+func TestSuccessLMSIdentical(t *testing.T) {
+	lml := make([]LuckyMoney, 0)
+	lml = append(lml, LuckyMoney{Quantity: 999, Amount: decimal.NewFromFloat(1000)})
+	lml = append(lml, LuckyMoney{Quantity: 9999, Amount: decimal.NewFromFloat(10000)})
+	lml = append(lml, LuckyMoney{Quantity: 333, Amount: decimal.NewFromFloat(30000)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(2000)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(1999.99)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(16)})
+	lml = append(lml, LuckyMoney{Quantity: 100, Amount: decimal.NewFromFloat(1600)})
+	lml = append(lml, LuckyMoney{Quantity: 1000, Amount: decimal.NewFromFloat(1600)})
+	lml = append(lml, LuckyMoney{Quantity: 1000, Amount: decimal.NewFromFloat(1000.01)})
+	lml = append(lml, LuckyMoney{Quantity: 1000, Amount: decimal.NewFromFloat(1001.99)})
+	lml = append(lml, LuckyMoney{Quantity: 99, Amount: decimal.NewFromFloat(19800)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(1)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(200)})
+	lml = append(lml, LuckyMoney{Quantity: 3, Amount: decimal.NewFromFloat(4)})
+	for _, lm := range lml {
+		a := lm.Amount
+		split, err := SplitIdentical(lm)
+		if err != nil {
+			t.Errorf("拆分红包失败 %s %s", lm.String(), err)
+			continue
+		}
+		sum := decimal.Zero
+		for _, d := range split {
+			if d.GreaterThan(MaxAmount) || d.LessThan(MinAmount) {
+				t.Errorf("红包金额异常 %s %s", lm.String(), d)
+			}
+			sum = sum.Add(d)
+		}
+		if !a.Equal(sum) {
+			t.Errorf("红包拆分异常 %s != %s", sum, a)
+		}
+	}
+}
+
+func TestErrorLMSIdentical(t *testing.T) {
+	lml := make([]LuckyMoney, 0)
+	lml = append(lml, LuckyMoney{Quantity: -1, Amount: decimal.NewFromFloat(3)})
+	lml = append(lml, LuckyMoney{Quantity: 0, Amount: decimal.NewFromFloat(10)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(0)})
+	lml = append(lml, LuckyMoney{Quantity: 1, Amount: decimal.NewFromFloat(-1.01)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(0.89)})
+	lml = append(lml, LuckyMoney{Quantity: 3, Amount: decimal.NewFromFloat(2.99)})
+	lml = append(lml, LuckyMoney{Quantity: 100, Amount: decimal.NewFromFloat(99.999)})
+	lml = append(lml, LuckyMoney{Quantity: 9, Amount: decimal.NewFromFloat(1801)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(2500)})
+	lml = append(lml, LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(2001)})
+	lml = append(lml, LuckyMoney{Quantity: 15, Amount: decimal.NewFromFloat(3001)})
+	fn := 0
+	for _, lm := range lml {
+		_, err := SplitIdentical(lm)
+		if err != nil {
+			fn++
+		}
+	}
+	if fn != len(lml) {
+		t.Errorf("拆分红包失败: 预期失败用例 %d，实际失败用例 %d", fn, len(lml))
+	}
+}
+
+func BenchmarkLMSIdentical10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(33)}
+		_, _ = SplitIdentical(lm)
+	}
+}
+
+func BenchmarkLMSIdentical100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 100, Amount: decimal.NewFromFloat(333)}
+		_, _ = SplitIdentical(lm)
+	}
+}
+
+func BenchmarkLMSIdentical1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 1000, Amount: decimal.NewFromFloat(3333)}
+		_, _ = SplitIdentical(lm)
+	}
+}
+func BenchmarkLMSIdentical10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 10000, Amount: decimal.NewFromFloat(33333)}
+		_, _ = SplitIdentical(lm)
+	}
+}
+func BenchmarkLMSIdentical100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 100000, Amount: decimal.NewFromFloat(333333)}
+		_, _ = SplitIdentical(lm)
+	}
+}
+
+func BenchmarkLMSRandom10(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 10, Amount: decimal.NewFromFloat(19)}
+		_, _ = SplitRandom(lm)
+	}
+}
+
+func BenchmarkLMSRandom100(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 100, Amount: decimal.NewFromFloat(199)}
+		_, _ = SplitRandom(lm)
+	}
+}
+
+func BenchmarkLMSRandom1000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 1000, Amount: decimal.NewFromFloat(1999)}
+		_, _ = SplitRandom(lm)
+	}
+}
+
+func BenchmarkLMSRandom10000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 10000, Amount: decimal.NewFromFloat(19999)}
+		_, _ = SplitRandom(lm)
+	}
+}
+
+func BenchmarkLMSRandom100000(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		lm := LuckyMoney{Quantity: 100000, Amount: decimal.NewFromFloat(199999)}
+		_, _ = SplitRandom(lm)
+	}
+}
